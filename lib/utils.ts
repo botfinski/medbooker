2fc8b3ff5e72309599f466748b402790cee37a6f
unknown <botfinski@gmail.com>
2024-07-10 01:55:57 +0200

    patient success page finished

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,60 @@ export function cn(...inputs: ClassValue[]) {
 export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
 
 export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
+
+export const formatDateTime = (dateString: Date | string) => {
+	const dateTimeOptions: Intl.DateTimeFormatOptions = {
+		month: "short",
+		day: "numeric",
+		year: "numeric",
+		hour: "numeric",
+		minute: "numeric",
+		hour12: true,
+	};
+
+	const dateDayOptions: Intl.DateTimeFormatOptions = {
+		weekday: "short",
+		year: "numeric",
+		month: "2-digit",
+		day: "2-digit",
+	};
+
+	const dateOptions: Intl.DateTimeFormatOptions = {
+		month: "short",
+		year: "numeric",
+		day: "numeric",
+	};
+
+	const timeOptions: Intl.DateTimeFormatOptions = {
+		hour: "numeric",
+		minute: "numeric",
+		hour12: true,
+	};
+
+	const formattedDateTime: string = new Date(dateString).toLocaleString(
+		"en-US",
+		dateTimeOptions
+	);
+
+	const formattedDateDay: string = new Date(dateString).toLocaleString(
+		"en-US",
+		dateDayOptions
+	);
+
+	const formattedDate: string = new Date(dateString).toLocaleString(
+		"en-US",
+		dateOptions
+	);
+
+	const formattedTime: string = new Date(dateString).toLocaleString(
+		"en-US",
+		timeOptions
+	);
+
+	return {
+		dateTime: formattedDateTime,
+		dateDay: formattedDateDay,
+		dateOnly: formattedDate,
+		timeOnly: formattedTime,
+	};
+};
