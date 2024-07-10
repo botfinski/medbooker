0473f8c67bbc4673540b0078c5627f3a970b7375
unknown <botfinski@gmail.com>
2024-07-10 16:01:42 +0200

    admin passkeyModal finished

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -49,6 +49,15 @@ const config = {
 			fontFamily: {
 				sans: ["var(--font-sans)", ...fontFamily.sans],
 			},
+			keyframes: {
+				"caret-blink": {
+					"0%,70%,100%": { opacity: "1" },
+					"20%,50%": { opacity: "0" },
+				},
+			},
+			animation: {
+				"caret-blink": "caret-blink 1.25s ease-out infinite",
+			},
 		},
 	},
 } satisfies Config;
